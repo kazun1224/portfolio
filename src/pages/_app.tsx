b233da4bf06c819e102bad727ee75f2b222bc5ee
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-07-20 18:07:24 +0900

    mantineを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import "src/styles/globals.css";
-import type { AppPropsWithLayout } from "next/app";
+import type { CustomAppPage  } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
+const MyApp: CustomAppPage = ({ Component, pageProps } ) => {
+  const getLayout =
+  Component.getLayout ||
+  ((page) => {
+    return page;
+  });
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <MantineProvider
+      theme={{
+        // Override any other properties from default theme
+        fontFamily: 'Open Sans, sans serif',
+        spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+      }}
+      >
+      {getLayout(<Component {...pageProps} />)}
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
