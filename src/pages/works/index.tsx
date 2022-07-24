e207298398541354248639720c0f1d581be6b497
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-07-24 23:53:21 +0900

    Workpageのレイアウトテスト

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,30 +1,87 @@
 import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import Image from "next/image";
 import { client } from "src/libs/client";
 import { Works, PropsList } from "src/types/works";
 
 const Works: NextPage<PropsList> = (props) => {
   return (
-    <div>
-      <h1 className="text-9xl">Works</h1>
-      <p>{props.totalCount}</p>
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        {/* <!-- text - start --> */}
+        <div className="mb-10 md:mb-16">
+          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+            Works
+          </h2>
 
-      <article>
-        <ul>
+          <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+            This is a section of some simple filler text, also known as
+            placeholder text. It shares some characteristics of a real written
+            text but is random or otherwise generated.
+          </p>
+        </div>
+        {/* <!-- text - end --> */}
+
+        <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
           {props.contents.map((content) => {
             return (
-              <li key={content.id}>
-                <h2>
-                  <Link href={`/works/${content.id}`}>
-                    <a className="underline">{content.title}</a>
-                  </Link>
-                </h2>
-              </li>
+              <div key={content.id}>
+                <Link href={`/works/${content.id}`}>
+                  <a className="flex flex-col overflow-hidden rounded-lg border bg-white">
+                    <div className="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
+                      <img
+                        src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
+                        loading="lazy"
+                        alt="Photo by Minh Pham"
+                        className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+                      />
+                    </div>
+
+                    <div className="flex flex-1 flex-col p-4 sm:p-6">
+                      <h2 className="mb-2 text-lg font-semibold text-gray-800">
+                        <a
+                          href="#"
+                          className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                        >
+                          {content.title}
+                        </a>
+                      </h2>
+
+                      <p className="mb-8 text-gray-500">
+                        This is a section of some simple filler text, also known
+                        as placeholder text. It shares some characteristics of a
+                        real written text.
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+              </div>
             );
           })}
-        </ul>
-      </article>
+        </div>
+      </div>
     </div>
+
+    // <div>
+    //   <h1 className="text-9xl">Works</h1>
+    //   <p>{props.totalCount}</p>
+
+    //   <article>
+    //     <ul>
+    //       {props.contents.map((content) => {
+    //         return (
+    //           <li key={content.id}>
+    //             <h2>
+    //               <Link href={`/works/${content.id}`}>
+    //                 <a className="underline">{content.title}</a>
+    //               </Link>
+    //             </h2>
+    //           </li>
+    //         );
+    //       })}
+    //     </ul>
+    //   </article>
+    // </div>
   );
 };
 
