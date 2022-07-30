40505ad45127a1e755f722ba1bf31c9c3cf71173
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-07-30 17:35:46 +0900

    NAVの要素にをフォルダー分け

---
+++ src/utils/nav.tsx
@@ -0,0 +1,5 @@
+export const NAV_ITEMS = [
+  { href: "/about", label: "About", active: false },
+  { href: "/works", label: "Works", active: false },
+  { href: "/contact", label: "Contact", active: false },
+] as const;
