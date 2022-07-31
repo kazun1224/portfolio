a3700da732beb22c15ccb6bd7339e1897720ad6c
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-07-31 14:47:24 +0900

    ✨ Drawerを作成

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
-
-const NAV_ITEMS = [
-  { href: "/about", label: "About" },
-  { href: "/works", label: "Works" },
-  { href: "/contact", label: "Contact" },
-] as const;
+import { useState } from "react";
+import { NAV_ITEMS } from "src/utils/nav";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   return (
+    <>
     <header className=" flex items-center justify-between py-4  md:py-8 ">
       {/* logo - start */}
       <Link href="/">
@@ -35,6 +38,7 @@ export const Header = () => {
       <button
         type="button"
         className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-green-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
+        onClick={toggleDrawer}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -52,7 +56,27 @@ export const Header = () => {
         </svg>
         Menu
       </button>
+
       {/* buttons - end */}
     </header>
+    <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="right"
+        className="bla bla bla"
+      >
+        <nav className="">
+          {NAV_ITEMS.map((item) => {
+            return (
+              <Link key={item.href} href={item.href}>
+                <a className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-green-500 active:text-green-700" onClick={toggleDrawer}>
+                  {item.label}
+                </a>
+              </Link>
+            );
+          })}
+        </nav>
+      </Drawer>
+    </>
   );
 };
