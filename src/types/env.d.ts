e31e6a2e4ef2d8ad5ff6c4d8d5cec171c72d669b
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-08-16 16:29:29 +0900

    ✨ お問い合わせ機能の実装

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,5 +4,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly SERVICE_DOMAIN: string;
     readonly API_KEY: string;
+    readonly SEND_GRIDE_MAIL_API_KEY: string;
+    readonly MY_MAIL_ADDRESS: string;
   }
 }
